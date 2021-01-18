9db1234ab54b674b294adf0ef85d3e9e5c5bb9d0
Corian2108 <jefferson.ruiz2108@gmail.com>
2021-01-18 16:50:45 -0500

    Se agrega todo el juego

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour {
+    public Rigidbody2D RbBall;
+    public Transform paddle;
+    public bool gameStarted;
+    public AudioSource ballAudio;
+    float posDif = 0;
+    // Start is called before the first frame update
+    void Start () {
+        posDif = paddle.position.x - transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!gameStarted) {
+            transform.position = new Vector3 (paddle.position.x - posDif, paddle.position.y, paddle.position.z);
+            if (Input.GetMouseButtonDown (0)) {
+                RbBall.velocity = new Vector2 (8, 8);
+                gameStarted = true;
+            }
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D ball) 
+    {
+        ballAudio.Play();
+    }
+}
---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DeadZone : MonoBehaviour {
+    public Text scorePlayerText;
+    public Text scoreEnemyText;
+    public SceneChanger sceneChanger;
+    public AudioSource pointAudio;
+
+    public bool scored;
+
+    int scorePlayerQuantity;
+    int scoreEnemyQuantity;
+
+    private void OnTriggerEnter2D (Collider2D collision) {
+        if (gameObject.tag == "Left") {
+            scoreEnemyQuantity++;
+            UpdateScoreLabel (scoreEnemyText, scoreEnemyQuantity);
+        } else if (gameObject.CompareTag ("Right")) {
+            scorePlayerQuantity++;
+            UpdateScoreLabel (scorePlayerText, scorePlayerQuantity);
+            scored = true;
+        }
+
+        collision.GetComponent<BallBehaviour>().gameStarted = false;
+        scored = false;
+        CheckScore();
+        pointAudio.Play();
+    }
+
+    void CheckScore () {
+        if (scorePlayerQuantity == 3) {
+            sceneChanger.ChangeSceneTo ("WinScene");
+        } else if (scoreEnemyQuantity == 3) {
+            sceneChanger.ChangeSceneTo ("LoseScene");
+        }
+    }
+
+    void UpdateScoreLabel (Text label, int score) {
+        label.text = score.ToString ();
+    }
+}
